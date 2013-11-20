73f6c3eb8ba39ad302fa239f175c9f99310fe631
Trevor Cooper <tcooper@sdsc.edu>
2013-11-20 15:00:00 -0800

    Update Makefile and version.mk for pull.mk

--- src/bbftpd/version.mk
+++ src/bbftpd/version.mk
@@ -1,3 +1,15 @@
-NAME    = bbftpd
-VERSION = 3.2.0
-RELEASE = 1
+NAME               = bbftpd
+VERSION            = 3.2.0
+RELEASE            = 1
+
+PKG_ROOT           = /opt/bbftp
+
+SRC_SUBDIR         = bbftpd
+
+BBFTPD_NAME        = $(NAME)
+BBFTPD_VERSION     = $(VERSION)
+BBFTPD_PKG_SUFFIX  = tgz
+BBFTPD_SOURCE_PKG  = $(BBFTPD_NAME)-$(BBFTPD_VERSION).$(BBFTPD_PKG_SUFFIX)
+BBFTPD_SOURCE_DIR  = $(BBFTPD_SOURCE_PKG:%.$(BBFTPD_PKG_SUFFIX)=%)
+
+TGZ_PKGS           = $(BBFTPD_SOURCE_PKG)
