ce06d70422a19d9d429cf115baaabd8b3c08e917
Trevor Cooper <tcooper@sdsc.edu>
2013-11-20 14:59:10 -0800

    Update Makefile and version.mk for pull.mk

--- src/bbcp/version.mk
+++ src/bbcp/version.mk
@@ -1,3 +1,14 @@
-NAME    = bbcp
-VERSION = 12.08.17
-RELEASE = 0
+NAME             = bbcp
+VERSION          = 12.08.17
+RELEASE          = 0
+PKG_ROOT         = /opt/bbcp
+
+SRC_SUBDIR       = bbcp
+
+BBCP_NAME        = $(NAME)
+BBCP_VERSION     = $(VERSION)
+BBCP_PKG_SUFFIX  = tgz
+BBCP_SOURCE_PKG  = $(BBCP_NAME)-$(BBCP_VERSION).$(BBCP_PKG_SUFFIX)
+BBCP_SOURCE_DIR  = $(BBCP_SOURCE_PKG:%.$(BBCP_PKG_SUFFIX)=%)
+
+TGZ_PKGS         = $(BBCP_SOURCE_PKG)
