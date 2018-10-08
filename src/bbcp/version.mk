31ca1d40ea98f8a97030e84fbd48760191e78d46
Jerry Greenberg <jpg@gordon-ln1.sdsc.edu>
2018-10-08 14:28:54 -0700

    APS-862  bbcp 17.12.00.00.0

--- src/bbcp/version.mk
+++ src/bbcp/version.mk
@@ -1,6 +1,6 @@
 NAME           = sdsc-bbcp
-VERSION        = 14.09.02.00.0
-RELEASE        = 1
+VERSION        = 17.12.00.00.0
+RELEASE        = 0
 PKGROOT        = /opt/bbcp
 
 SRC_SUBDIR     = bbcp
@@ -9,7 +9,7 @@ SOURCE_NAME    = bbcp
 SOURCE_SUFFIX  = tgz
 SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
-SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+SOURCE_DIR     = $(SOURCE_NAME)
 
 TGZ_PKGS       = $(SOURCE_PKG)
 
